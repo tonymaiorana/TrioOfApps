6c1470a9e96f6b0556cc5349f06ff3e8a99702d4
Kirkland Brown <kizzet373@gmail.com>
2016-04-08 11:38:51 -0400

    JKKTF-17 - Added a few more methods to the DVDRepository and DVDOperations

---
+++ DVDLibrary/DvdLibrary.Models/Studio.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DvdLibrary.Models
+{
+    public class Studio
+    {
+        public int StudioId { get; set; }
+        public string StudioName { get; set; }
+    }
+}
