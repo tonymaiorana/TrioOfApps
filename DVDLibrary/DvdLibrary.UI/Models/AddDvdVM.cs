035b196ac97e859526e084f4912e6b861a5dac03
Hyun Jeong Hong <hisuida@me.com>
2016-04-08 17:07:18 -0400

    JKKTF-6 Enum showing

--- DVDLibrary/DvdLibrary.UI/Models/AddDvdVM.cs
+++ DVDLibrary/DvdLibrary.UI/Models/AddDvdVM.cs
@@ -1,30 +1,30 @@
-﻿using System;
+﻿using DvdLibrary.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using DvdLibrary.Models;
 
 namespace DvdLibrary.UI.Models
 {
     public class AddDvdVM
     {
-        public Dvd newDvd { get; set; }
-        public List<Dvd> Dvds { get; set; }
-        public MPAARating MpaaRating;
-        public List<SelectListItem> MpaaDropDown { get; set; }
+        //Can get rid of potentially, since we solved the ENUM issue.
 
-        public AddDvdVM(List<Dvd> dvds)
-        {
-            MpaaDropDown = new List<SelectListItem>();
-            foreach (var rating in Enum.GetValues(typeof(MPAARating)))
-            {
-                var ListItem = new SelectListItem {Text = rating.ToString(), Value = rating.ToString()};
-               MpaaDropDown.Add(ListItem);
-            }
-           
+        //public Dvd newDvd { get; set; }
+        //public List<Dvd> Dvds { get; set; }
+        //public MPAARating MpaaRating;
+        //public List<SelectListItem> MpaaDropDown { get; set; }
 
-        }
+        //public AddDvdVM(List<Dvd> dvds)
+        //{
+        //    MpaaDropDown = new List<SelectListItem>();
+        //    foreach (var rating in Enum.GetValues(typeof(MPAARating)))
+        //    {
+        //        var ListItem = new SelectListItem {Text = rating.ToString(), Value = rating.ToString()};
+        //       MpaaDropDown.Add(ListItem);
+        //    }
 
+        //}
     }
 }
