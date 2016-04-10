12143246cb09019262e08dac4103db8ebf46268f
Hyun Jeong Hong <hisuida@me.com>
2016-04-10 13:25:00 -0400

    JKKTF-11 Updated borrower view, borrowinfo view

--- DVDLibrary/DvdLibrary.Models/Borrower.cs
+++ DVDLibrary/DvdLibrary.Models/Borrower.cs
@@ -1,17 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DvdLibrary.Models
 {
-    public class Borrower
+    public class Borrower : IValidatableObject
     {
         public int BorrowerId { get; set; }
         public string BorrowerFirstName { get; set; }
         public string BorrowerLastName { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
         public string BorrowerPhoneNumber { get; set; }
+
         public bool IsActive { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            List<ValidationResult> errors = new List<ValidationResult>();
+
+            if (string.IsNullOrEmpty(BorrowerFirstName))
+            {
+                errors.Add(new ValidationResult("Please enter your first name", new[] { "First Name" }));
+            }
+            if (string.IsNullOrEmpty(BorrowerLastName))
+            {
+                errors.Add(new ValidationResult("Please enter your last name", new[] { "Last Name" }));
+            }
+            if (string.IsNullOrEmpty(BorrowerPhoneNumber))
+            {
+                errors.Add(new ValidationResult("Please enter a valid phone number", new[] { "Phone Number" }));
+            }
+
+            return errors;
+        }
     }
 }
