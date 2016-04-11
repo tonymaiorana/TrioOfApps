0a5d59569e095767175b129f87e768165e108fda
Hyun Jeong Hong <hisuida@me.com>
2016-04-11 14:20:13 -0400

    JKKTF-11, JKKTF-12 added Borrower add (register), edit, delete

---
+++ DVDLibrary/DvdLibrary.BLL/ValidateBorrower.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DvdLibrary.BLL
+{
+    public class ValidateBorrower
+    {
+        public bool IsValid(string LastName, string PhoneNumber)
+        {
+            using (
+                var cn =
+                    new SqlConnection(
+                        @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|Database1.mdf;Integrated Security=True")
+                )
+            {
+                string _sql = @"SELECT [Username] FROM [dbo].[System_Users] " +
+                              @"WHERE [Username] = @u AND [Password] = @p";
+                var cmd = new SqlCommand(_sql, cn);
+                cmd.Parameters
+                    .Add(new SqlParameter("@u", SqlDbType.NVarChar))
+                    .Value = LastName;
+                cmd.Parameters
+                    .Add(new SqlParameter("@p", SqlDbType.NVarChar))
+                    .Value = PhoneNumber;
+                cn.Open();
+                var reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    reader.Dispose();
+                    cmd.Dispose();
+                    return true;
+                }
+                else
+                {
+                    reader.Dispose();
+                    cmd.Dispose();
+                    return false;
+                }
+            }
+        }
+    }
+}
