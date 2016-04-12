ea96c93dbb90a0d4b52df5da63e5b5575a221628
Hyun Jeong Hong <hisuida@me.com>
2016-04-11 20:17:12 -0400

    JKKTF-11,JKKTF-12

---
+++ DVDLibrary/DvdLibrary.UI/Models/ListDvdVM.cs
@@ -0,0 +1,16 @@
+﻿using DvdLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DvdLibrary.UI.Models
+{
+    public class ListDvdVM
+    {
+        public List<Dvd> Dvds { get; set; }
+
+        //public List<Borrower> Borrowers { get; set; }
+        public Borrower currentBorrower { get; set; }
+    }
+}
