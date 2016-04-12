64bccba2310669c7175edf0b8442ab4529367441
Hyun Jeong Hong <hisuida@me.com>
2016-04-12 13:29:36 -0400

    JKKTF-12

---
+++ DVDLibrary/DvdLibrary.UI/Models/BorrowInfoVM.cs
@@ -0,0 +1,14 @@
+﻿using DvdLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DvdLibrary.UI.Models
+{
+    public class BorrowInfoVM
+    {
+        public List<Dvd> Dvds { get; set; }
+        public List<BorrowInfo> BorrowInfos { get; set; }
+    }
+}
