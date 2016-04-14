b340304b1c3c674fb1b374052e7fcd6e5c0e1dab
Kazuhiro Nishimoto <kazuhiro.nishimoto@gmail.com>
2016-04-14 13:43:22 -0400

    JKKTF-19 started testing

--- DVDLibrary/DvdLibrary.Tests/DataTests.cs
+++ DVDLibrary/DvdLibrary.Tests/DataTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DvdLibrary.Data;
 using NUnit.Framework;
 
 namespace DvdLibrary.Tests
@@ -11,6 +12,9 @@ namespace DvdLibrary.Tests
     class DataTests
     {
         [TestCase()]
-        public void 
+        public void getStudioName(string studio)
+        {
+
+        }
     }
 }
