e922dd6d97f9da5b5aa48c2bc833a269c5af5e06
Kazuhiro Nishimoto <kazuhiro.nishimoto@gmail.com>
2016-04-15 18:50:40 -0400

    JKKTF-15 added testing

--- DVDLibrary/DvdLibrary.Tests/DataTests.cs
+++ DVDLibrary/DvdLibrary.Tests/DataTests.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DvdLibrary.Data;
+using DvdLibrary.Models;
 using NUnit.Framework;
 
 namespace DvdLibrary.Tests
@@ -13,10 +14,20 @@ namespace DvdLibrary.Tests
     {
         private MockDvdRepository _Mock = new MockDvdRepository();
 
-        [TestCase()]
-        public void GetStudioName(string studio)
+        [TestCase(1, "Pokemon 2000", 1, 1, 1)]
+        public void GetDvdById_Test(int dvdId, string expectedTitle, int expectedBorrowInfoId, int expectedBorrowerId, int expectedDirectorId)
         {
-            
+            Assert.AreEqual(expectedTitle, _Mock.GetDvdById(dvdId).Title);
+            Assert.AreEqual(expectedBorrowInfoId, _Mock.GetDvdById(dvdId).BorrowInfo.BorrowInfoId);
+            Assert.AreEqual(expectedBorrowerId, _Mock.GetDvdById(dvdId).BorrowInfo.Borrower.BorrowerId);
+            Assert.AreEqual(expectedDirectorId, _Mock.GetDvdById(dvdId).Director.DirectorId);
+        }
+
+        [TestCase("Warner", 1, "Warner")]
+        public void GetStudioByName_Test2(string studioName, int expectedStudioId, string expectedStudioName)
+        {
+                Assert.AreEqual(expectedStudioName, _Mock.GetStudioByName(studioName).StudioName);
+                Assert.AreEqual(expectedStudioId, _Mock.GetStudioByName(studioName).StudioId);          
         }
     }
 }
