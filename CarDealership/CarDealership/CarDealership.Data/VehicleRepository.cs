27ce20dd1d3bae5fba2ee95e349cac0ac2187466
Hyun Jeong Hong <hisuida@me.com>
2016-04-17 11:45:11 -0400

    JKKTS-10 created vehicle repo, requestform repo

---
+++ CarDealership/CarDealership/CarDealership.Data/VehicleRepository.cs
@@ -0,0 +1,47 @@
+﻿using CarDealership.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealership.Data
+{
+    internal class VehicleRepository : IVehicleRepository
+    {
+        private List<Vehicle> Cars = new List<Vehicle>();
+
+        private string constr;
+
+        public VehicleRepository()
+        {
+            constr = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        }
+
+        public List<Vehicle> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Vehicle GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Vehicle AddBorrower(Vehicle model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int id, Vehicle model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
