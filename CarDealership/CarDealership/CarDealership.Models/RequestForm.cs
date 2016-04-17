2eb8642d7545e34b0979ad16e3cbb063176f94a9
Kirkland Brown <kizzet373@gmail.com>
2016-04-16 20:08:35 -0400

    JKKTS-2 - Updated Class Model Diagram, Created Application Layers, and added Models

---
+++ CarDealership/CarDealership/CarDealership.Models/RequestForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealership.Models
+{
+    public class RequestForm
+    {
+        public int VehicleId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
+        public string BestTimeToCall { get; set; } //TextBox
+        public PreferedContactMethod PreferedContactMethod { get; set; } //Radio Buttons or Dropdown
+        public DateTime DateNeedToPurchaseBy { get; set; } //EditorFor
+        public string AdditionalInfo { get; set; }
+        public DateTime LastContacted { get; set; } //EditorFor
+        public RequestFormStatus RequestFormStatus { get; set; } //DropDown
+        public int UserAccountId { get; set; }
+    }
+}
---
+++ CarDealership/CarDealership/CarDealership.Models/Vehicle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealership.Models
+{
+    public class Vehicle
+    {
+        public int VehicleId { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public DateTime Year { get; set; }
+        public int Mileage { get; set; }
+        public string AdTitle { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public string PictureUrl { get; set; }
+        public bool IsAvailable { get; set; }
+        public bool IsNew { get; set; }
+        public int AdminAccountId { get; set; }
+    }
+}
