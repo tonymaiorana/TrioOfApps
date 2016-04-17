ba1e3472803ac47c5c443c53c9563d605e1ee552
Kazuhiro Nishimoto <kazuhiro.nishimoto@gmail.com>
2016-04-17 18:33:32 -0400

    JKKTS-6 migrated methods for 5, 6, 7

---
+++ CarDealership/CarDealership/CarDealership.Data/CarDealershipRepository.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarDealership.Models;
+
+namespace CarDealership.Data
+{
+    public class CarDealershipRepository
+    {
+        private static List<RequestForm> _requestForms = new List<RequestForm>();
+
+        public CarDealershipRepository()
+        {
+            if (!_requestForms.Any())
+            {
+                _requestForms.AddRange(new List<RequestForm>()
+                {
+                    new RequestForm()
+                    {
+                        VehicleId = 1,
+                        FirstName = "Victor",
+                        LastName = "Pudelski",
+                        EmailAddress = "victor@swcguild.com",
+                        PhoneNumber = "123-4567",
+                        BestTimeToCall = "7 pm",
+                        PreferedContactMethod = PreferedContactMethod.PhoneCall,
+                        DateNeedToPurchaseBy = DateTime.Now,
+                        AdditionalInfo = "Cash payment",
+                        LastContacted = DateTime.Now,
+                        RequestFormStatus = RequestFormStatus.AwaitingReply,
+                        UserAccountId = 1
+                    },
+
+                    new RequestForm()
+                    {
+                        VehicleId = 2,
+                        FirstName = "Kaz",
+                        LastName = "Tanaka",
+                        EmailAddress = "victor@swcguild.com",
+                        PhoneNumber = "123-9876",
+                        BestTimeToCall = "7 pm",
+                        PreferedContactMethod = PreferedContactMethod.PhoneCall,
+                        DateNeedToPurchaseBy = DateTime.Now,
+                        AdditionalInfo = "Cash payment",
+                        LastContacted = DateTime.Now,
+                        RequestFormStatus = RequestFormStatus.AwaitingReply,
+                        UserAccountId = 2
+                    },
+
+                    new RequestForm()
+                    {
+                        VehicleId = 1,
+                        FirstName = "Kirk",
+                        LastName = "Brown",
+                        EmailAddress = "victor@swcguild.com",
+                        PhoneNumber = "456-4567",
+                        BestTimeToCall = "7 pm",
+                        PreferedContactMethod = PreferedContactMethod.PhoneCall,
+                        DateNeedToPurchaseBy = DateTime.Now,
+                        AdditionalInfo = "Cash payment",
+                        LastContacted = DateTime.Now,
+                        RequestFormStatus = RequestFormStatus.AwaitingReply,
+                        UserAccountId = 3
+                    }
+                });
+            }
+        }
+
+
+        public List<RequestForm> GetAllRequestForms()
+        {
+            return _requestForms;
+        }
+
+        public RequestForm GetRequestFormById(int id)
+        {
+            return _requestForms.FirstOrDefault(f => f.VehicleId == id);
+        }
+
+        public void Add(RequestForm newForm)
+        {
+            newForm.VehicleId = (_requestForms.Any()) ? _requestForms.Max(f => f.VehicleId) + 1 : 1;
+
+            _requestForms.Add(newForm);
+        }
+
+        public void Delete(int id)
+        {
+            _requestForms.RemoveAll(f => f.VehicleId == id);
+        }
+
+        public void Edit(RequestForm form)
+        {
+            Delete(form.VehicleId);
+            _requestForms.Add(form);
+        }
+    }
+}
---
+++ CarDealership/CarDealership/CarDealership/Controllers/RequestFormController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CarDealership.Data;
+using CarDealership.Models;
+
+namespace CarDealership.Controllers
+{
+    public class RequestFormController : Controller
+    {
+        // GET: RequestForm
+        public ActionResult List()
+        {
+            var repo = new CarDealershipRepository();
+            var allForms = repo.GetAllRequestForms();
+            return View(allForms);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var repo = new CarDealershipRepository();
+            var form = repo.GetRequestFormById(id);
+            return View(form);
+        }
+
+        public ActionResult NewForm()
+        {
+            return View("NewForm",new RequestForm());
+        }
+
+        [HttpPost]
+        public ActionResult NewForm(RequestForm newFrom)
+        {
+            var repo = new CarDealershipRepository();
+            repo.Add(newFrom);
+
+            return RedirectToAction("List");
+        }
+
+        public ActionResult DeleteForm(int id)
+        {
+            var repo = new CarDealershipRepository();
+            var form = repo.GetRequestFormById(id);
+            return View(form);
+        }
+        
+        [HttpPost]
+        public ActionResult DeleteForm(RequestForm form)
+        {
+            var repo = new CarDealershipRepository();
+            repo.Delete(form.VehicleId);
+
+            return RedirectToAction("List");
+        }
+
+        public ActionResult EditForm(int id)
+        {
+            var repo = new CarDealershipRepository();
+            var form = repo.GetRequestFormById(id);
+            return View(form);
+        }
+        
+        [HttpPost]
+        public ActionResult EditForm(RequestForm form)
+        {
+            var repo = new CarDealershipRepository();
+            repo.Edit(form);
+
+            return RedirectToAction("List");
+        }
+    }
+}
