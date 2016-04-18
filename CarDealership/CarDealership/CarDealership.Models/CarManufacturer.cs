7137488c7b8d060240523a75b917f839c307490f
Kirkland Brown <kizzet373@gmail.com>
2016-04-18 11:50:56 -0400

    JKKTS-2, JKKTS-9 - Added Enum for Year and CarManufacturer, and SQL script to modify DateNeedToPurchaseBy. Also removed duplicate redundant folder in repository.

---
+++ CarDealership/CarDealership/CarDealership.Models/CarManufacturer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealership.Models
+{
+    public enum CarManufacturer
+    {
+        Honda,
+        Toyota,
+        Tesla,
+        Ford,
+        Chevrolet
+    }
+}
---
+++ CarDealership/CarDealership/CarDealership.Models/Year.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealership.Models
+{
+    public enum Year
+    {
+        A = 2017,
+        B = 2016,
+        C = 2015,
+        D = 2014,
+        E = 2013,
+        F = 2012,
+        G = 2011,
+        H = 2010,
+        I = 2009,
+        J = 2008,
+        K = 2007,
+        L = 2006,
+        M = 2005,
+        N = 2004,
+        O = 2003,
+        P = 2002,
+        Q = 2001,
+        R = 2000,
+        S = 1999,
+        T = 1998,
+        U = 1997,
+        V = 1996,
+        W = 1995,
+        X = 1994,
+    }
+}
